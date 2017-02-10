bc962d7c357b8b7dc2264075737bc20936b5944e
Lukas Micke <LukasMicke@ymail.com>
2017-02-10 18:27:58 +0100

    Command line added

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import(
   "fmt"
   "io/ioutil"
   "Sequence-analysis/bio"
+  "os"
 )
 
 func check(e error){
@@ -14,14 +15,26 @@ func check(e error){
 
 func main() {
   //f, err := os.Open("test.txt")
-  dat, err := ioutil.ReadFile("test.txt")
+  args := os.Args[1:]
+  dat, err := ioutil.ReadFile(args[1])
   check(err)
-  bio.CountNuc(string(dat))
-  fmt.Println("GC-Gehalt:", bio.GCContent(string(dat)))
-  fmt.Println("A:", bio.A)
-  fmt.Println("T:", bio.T)
-  fmt.Println("G:", bio.G)
-  fmt.Println("C:", bio.C)
+
+  switch method := args[0] ; method {
+  case "GCContent":
+    fmt.Println("GC-Gehalt:", bio.GCContent(string(dat)))
+  case "CountNuc":
+    bio.CountNuc(string(dat))
+    fmt.Println("A:", bio.A)
+    fmt.Println("T:", bio.T)
+    fmt.Println("G:", bio.G)
+    fmt.Println("C:", bio.C)
+  default:
+    fmt.Println("Keine Methode angegeben")
+
+  }
+
   
+
+
 
 }
