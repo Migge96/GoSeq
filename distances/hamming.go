0d9b092088d4a6134cd5b8d6a79ad5bc6d692d6e
Lukas Micke <lukasmicke96@gmail.com>
2019-07-28 14:35:41 +0200

    Refactored hamming distance and added tests

--- distances/hamming.go
+++ distances/hamming.go
@@ -1,11 +1,17 @@
 package distances
 
-func hamming(seqA, seqB string) int {
+import "errors"
+
+func hamming(seqA, seqB *string) (int, error) {
+	if len(*seqA) != len(*seqB) {
+		return 0, errors.New("Error: Unequal Length of strings")
+	}
+
 	difference := 0
-	for i, val := range seqA {
-		if string(val) != string(seqB[i]) {
+	for i, val := range *seqA {
+		if string(val) != string((*seqB)[i]) {
 			difference++
 		}
 	}
-	return difference
+	return difference, nil
 }
--- distances/hamming_test.go
+++ distances/hamming_test.go
@@ -3,14 +3,34 @@ package distances
 import "testing"
 
 func TestHamming(t *testing.T) {
-	//Test Cases
-	seqA := "Hallo"
-	seqB := "Hello"
 
-	want := 1
-	got := hamming(seqA, seqB)
+	t.Run("Two Strings of equal length", func(t *testing.T) {
+		//Test Cases
+		seqA := "CAG"
+		seqB := "CAA"
 
-	if got != want {
-		t.Errorf("Got %d, wanted %d", got, want)
-	}
+		want := 1
+		got, _ := hamming(&seqA, &seqB)
+
+		if got != want {
+			t.Errorf("Got %d, wanted %d", got, want)
+		}
+	})
+
+	t.Run("unequal lengths", func(t *testing.T) {
+		seqA := "CAG"
+		seqB := "CA"
+
+		want := "Error: Unequal Length of strings"
+		_, got := hamming(&seqA, &seqB)
+
+		if got == nil {
+			t.Error("want an error for unequal length")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+
+	})
 }
