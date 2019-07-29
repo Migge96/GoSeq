621640b0097ab1a3f043271c4937ecff17a4024a
Lukas Valentin Micke <lukasvalentin.micke@bertelsmann.de>
2019-07-29 11:16:24 +0200

    Working edit distance function in O(nm) in Time and Space

--- distances/editDistance.go
+++ distances/editDistance.go
@@ -1,5 +1,53 @@
 package distances
 
+// EditDistance computes the edit distance betwwen two strings a and b.
 func EditDistance(seqA, seqB string) int {
-	return -1
+	lenA := (len(seqA) + 1)
+	lenB := (len(seqB) + 1)
+	// Initiliaze the Edit Matrix
+	editMatrix := make([][]int, lenA)
+	for i := 0; i < lenA; i++ {
+		editMatrix[i] = make([]int, lenB)
+	}
+
+	for i := 0; i < lenA; i++ {
+		editMatrix[i][0] = i
+
+	}
+
+	for j := 0; j < lenB; j++ {
+		editMatrix[0][j] = j
+
+	}
+
+	for i := 1; i < lenA; i++ {
+		for j := 1; j < lenB; j++ {
+			vertical := editMatrix[i-1][j] + 1
+			horizontal := editMatrix[i][j-1] + 1
+			diagonal := editMatrix[i-1][j-1]
+
+			if seqA[i-1] != seqB[j-1] {
+				diagonal++
+			}
+
+			editMatrix[i][j] = minOf3(vertical, horizontal, diagonal)
+
+		}
+	}
+
+	return editMatrix[lenA-1][lenB-1]
+}
+
+func minOf3(a, b, c int) int {
+	min := a
+
+	if b < min {
+		min = b
+	}
+
+	if c < min {
+		min = c
+	}
+
+	return min
 }
--- distances/editDistance_test.go
+++ distances/editDistance_test.go
@@ -3,14 +3,44 @@ package distances
 import "testing"
 
 func TestEditDistance(t *testing.T) {
-	//Test Cases
-	seqA := "CAAG"
-	seqB := "CAGG"
+	t.Run("Simple DNA Example with len 5 and 6", func(t *testing.T) {
+		//Test Cases
+		seqA := "BCACD"
+		seqB := "DBADAD"
 
-	want := 1
-	got := EditDistance(seqA, seqB)
+		want := 4
+		got := EditDistance(seqA, seqB)
 
-	if want != got {
-		t.Errorf("got %d, wanted %d, given %q and %q", got, want, seqA, seqB)
-	}
+		if want != got {
+			t.Errorf("got %d, wanted %d, given %q and %q", got, want, seqA, seqB)
+		}
+
+	})
+
+	t.Run("One Sequence Empty Should be only insertions", func(t *testing.T) {
+		//Test Cases
+		seqA := "AAAAA"
+		seqB := ""
+
+		want := 5
+		got := EditDistance(seqA, seqB)
+
+		if want != got {
+			t.Errorf("got %d, wanted %d, given %q and %q", got, want, seqA, seqB)
+		}
+	})
+
+	t.Run("Simple english Sentence with Typo", func(t *testing.T) {
+		//Test Cases
+		seqA := "Hello, my name is Lukas."
+		seqB := "Hallo, my name is Lukas."
+
+		want := 1
+		got := EditDistance(seqA, seqB)
+
+		if want != got {
+			t.Errorf("got %d, wanted %d, given %q and %q", got, want, seqA, seqB)
+		}
+	})
+
 }
